8ab64dfb7f7491e6e9ea7ec01aa3041eda89e1a9
Parsa Mohammadpour <parsa1380p.m.p@gmail.com>
2022-11-06 22:54:51 +0330

    Normalizing Inputs.

--- main.py
+++ main.py
@@ -39,23 +39,33 @@ df = pd.read_csv('train.csv').iloc[:1000, :]
 # Converting Non Numeric Values To Numeric Values By Using one_hot_function
 city_rank = {}
 one_hot_encode(city_rank, df['City'])
-df['City Rank'] = [city_rank[x] for x in df['City']]
+rank = [city_rank[x] for x in df['City']]
+rank_norm = numpy.linalg.norm(rank)
+df['City Rank'] = [x / rank_norm for x in rank]
 
 state_rank = {}
 one_hot_encode(state_rank, df['State'])
-df['State Rank'] = [state_rank[x] for x in df['State']]
+rank = [state_rank[x] for x in df['State']]
+rank_norm = numpy.linalg.norm(rank)
+df['State Rank'] = [x / rank_norm for x in rank]
 
 vin_rank = {}
 one_hot_encode(vin_rank, df['Vin'])
-df['Vin Rank'] = [vin_rank[x] for x in df['Vin']]
+rank = [vin_rank[x] for x in df['Vin']]
+rank_norm = numpy.linalg.norm(rank)
+df['Vin Rank'] = [x / rank_norm for x in rank]
 
 make_rank = {}
 one_hot_encode(make_rank, df['Make'])
-df['Make Rank'] = [make_rank[x] for x in df['Make']]
+rank = [make_rank[x] for x in df['Make']]
+rank_norm = numpy.linalg.norm(rank)
+df['Make Rank'] = [x / rank_norm for x in rank]
 
 model_rank = {}
 one_hot_encode(model_rank, df['Model'])
-df['Model Rank'] = [model_rank[x] for x in df['Model']]
+rank = [model_rank[x] for x in df['Model']]
+rank_norm = numpy.linalg.norm(rank)
+df['Model Rank'] = [x / rank_norm for x in rank]
 
 columns = ['Year', 'Mileage', 'City Rank', 'State Rank', 'Vin Rank', 'Make Rank', 'Model Rank']
 x_vector = df.loc[:, columns].to_numpy()
@@ -71,10 +81,11 @@ print('target:')
 print(target)
 
 learning_rate = 0.0000000001
-epoch = 10000
+epoch = 100000
 
 # Setting The Bias Number In This Vector Along Side Of Xi
-w = [((random.random() * 0.0001)) for x in range(len(columns) + 1)]
+w = [((random.random() * 1)) for x in range(len(columns) + 1)]
+w = [((random.random() * 1)) for x in range(len(columns) + 1)]
 print('W:')
 print(w)
 
@@ -93,8 +104,8 @@ for i in range(epoch):
     print('New W:')
     print(w)
     predict = np.dot(x_vector, w)
-    print('Predict:')
-    print(predict)
+    # print('Predict:')
+    # print(predict)
     cost = cost_function_simplified(predict, target)
     print('cost:')
     print(cost)
@@ -112,25 +123,30 @@ plt.show()
 
 ###################################################################
 ##############         My Test          ####################
-# df = pd.read_csv('train.csv')
-# df = pd.read_csv('train.csv').iloc[1100:1110, :]
-#
-# # Converting Non Numeric Values To Numeric Values By Using rank function
-# df['City Rank'] = [one_hot_decode(city_rank, x) for x in df['City']]
-# df['State Rank'] = [one_hot_decode(state_rank, x) for x in df['State']]
-# df['Vin Rank'] = [one_hot_decode(vin_rank, x) for x in df['Vin']]
-# df['Make Rank'] = [one_hot_decode(make_rank, x) for x in df['Make']]
-# df['Model Rank'] = [one_hot_decode(model_rank, x) for x in df['Model']]
-#
-# test_x = df.loc[:, columns].to_numpy()
-# test_bias = np.ones(len(df))
-# test_x = np.c_[test_x, test_bias]
-#
-# df['Predicted Price'] = numpy.dot(test_x, w)
-# plt.plot([x for x in range(len(df))], np.array(df.iloc[:,0].values), color='b')
-# plt.plot([x for x in range(len(df))], np.array(df.loc[:,['Predicted Price']].values), color='r')
-# plt.savefig('My Test On 10 Test date.png')
-# plt.show()
+df = pd.read_csv('train.csv')
+df = pd.read_csv('train.csv').iloc[1570:1590, :]
+
+# Converting Non Numeric Values To Numeric Values By Using rank function
+df['City Rank'] = [one_hot_decode(city_rank, x) for x in df['City']]
+df['State Rank'] = [one_hot_decode(state_rank, x) for x in df['State']]
+df['Vin Rank'] = [one_hot_decode(vin_rank, x) for x in df['Vin']]
+df['Make Rank'] = [one_hot_decode(make_rank, x) for x in df['Make']]
+df['Model Rank'] = [one_hot_decode(model_rank, x) for x in df['Model']]
+
+test_x = df.loc[:, columns].to_numpy()
+test_bias = np.ones(len(df))
+test_x = np.c_[test_x, test_bias]
+
+df['Predicted Price'] = numpy.dot(test_x, w)
+
+print('Total Predicted Price:')
+print(df['Predicted Price'].to_string())
+
+plt.plot([x for x in range(len(df))], np.array(df.iloc[:,0].values), color='b')
+plt.plot([x for x in range(len(df))], np.array(df.loc[:,['Predicted Price']].values), color='r')
+plt.savefig('My Test On 10 Test date.png')
+plt.show()
+exit(0)
 ###################################################################
 df = pd.read_csv('test.csv')
 # just in case that we can see the result, if we get all test.csv file, we get error of merror space due to its huge size
